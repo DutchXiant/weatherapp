181cdcbce990989f38f0d9202c00dcff3e20ac9d
Roy <rjong1033@gmaill.com>
2023-01-18 16:11:32 +0100

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     <>
     <div className='w-[85%] m-auto p-5 flex flex-col flex-1 justify-center items-center'>
        <h1 className='font-bold text-3xl flex'>Wat is MyFitCamp? </h1>
-       <section className='flex gap-3 sm:flex-col lg:flex-row md:flex-col'>
+       <section className='flex gap-3 sm:flex-col lg:flex-row md:flex-col xs:flex-col'>
           <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3 md:m-auto'>
             <p className='font-bold text-lg'>Gepersonaliseerde bodingsschema's</p>
             <p className="text-[#bcbcbc]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ducimus in harum ea. Sequi adipisci similique dolorum vitae laboriosam deserunt!</p>
