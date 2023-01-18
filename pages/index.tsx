03ad23f8fcef9e9daee4a02656211b1784686db4
Roy <rjong1033@gmaill.com>
2023-01-18 13:59:14 +0100

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-   <div>
-    Hello this is a test
-   </div>
+    <div className='w-[85%] m-auto p-5'>
+       <div className='flex flex-1 justify-center'>
+           <img src="https://i.ibb.co/RYVtpxz/undraw-Tasting-re-3k5a-removebg-preview.png"></img>
+       </div>
+       <div className='bg-white flex flex-1 flex-col p-5 shadow-xl gap-2'>
+           <h1 className='text-2xl font-extrabold'>Hoe werkt het?</h1>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nulla aspernatur dolor provident! Molestiae qui, sed aliquid quidem laudantium eaque.</p>
+           <button className="bg-[#FFA400] font-bold py-2">Nu proberen!</button>
+       </div>
+       
+    </div>
+
+
   )
 }
 
