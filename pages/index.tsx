01dc8515804f331c53888b1cb0ef88ec6711415f
Roy <rjong1033@gmaill.com>
2023-01-18 20:01:32 +0100

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import Hero from '../Components/Hero'
 
+
+
 const Home: NextPage = () => {
   return (
     <>
@@ -10,7 +12,7 @@ const Home: NextPage = () => {
        <section className='flex gap-3 sm:flex-col lg:flex-row md:flex-col xs:flex-col'>
           <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3 md:m-auto'>
             <p className='font-bold text-3xl text-white'>Gepersonaliseerd Trainings Schema🏋️ </p>
-            <p className="text-[#bcbcbc] text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ducimus in harum ea. Sequi adipisci similique dolorum vitae laboriosam deserunt!</p>
+            <p className="text-[#bcbcbc] text-lg">Ontvang binnen 1 minuut een professioneel trainingsschema afsteld op jouw fitness doelen.    </p>
             <button className=' bg-[#fba400] py-2 text-black font-bold'>Leer meer</button>
            </div>
            <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3  md:m-auto'>
@@ -31,8 +33,8 @@ const Home: NextPage = () => {
         {/* Section one  */}
        <section className='flex flex-row gap-9  p-5 xs:flex-col md:flex-row lg:flex-row'>
          <div className="flex-1   ">
-             <h1 className='font-bold text-3xl text-white'>Voedingsschema binnen 1 minuut!</h1>
-            <p className='text-[#bcbcbc] text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti sunt sint explicabo quia consequuntur? Minus quia quis similique hic ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, rem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, deserunt.</p>
+             <h1 className='font-bold text-3xl text-white'>Elke week een boodschappenlijstje📝</h1>
+            <p className='text-[#bcbcbc] text-lg'>Met MyFitCampPro krijg jij elke week jouw gepersonaliseerde boodschappen lijstje gestuurd in je e-mail. Dit lijstje bevat de voeding die jij nodig hebt om jouw fitness doel te behalen!  </p>
             
          </div>
          <div className='flex-1 rounded-xl shadow-3xl'><img  className='rounded-3xl w-[600px]  m-auto items-center shadow-3xl' src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></div>
@@ -40,10 +42,10 @@ const Home: NextPage = () => {
 
        {/* Section 2 */}
        <section className='flex flex-row gap-9  p-5 xs:flex-col md:flex-row lg:flex-row'>
-       <div className='flex-1 rounded-xl shadow-3xl'><img  className='rounded-3xl w-[600px]  m-auto items-center shadow-3xl' src="https://images.pexels.com/photos/8154267/pexels-photo-8154267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></div>
+       <div className='flex-1 rounded-xl shadow-3xl xs:order-1 md:order-none'><img  className='rounded-3xl w-[600px]  m-auto items-center shadow-3xl' src="https://images.pexels.com/photos/8154267/pexels-photo-8154267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></div>
          <div className="flex-1   ">
-             <h1 className='font-bold text-3xl text-white'>En een gratis trainingsschema!</h1>
-            <p className='text-[#bcbcbc]  text-lg'>Naast een gratis voedingsschema krijg je ook nog een gratis trainingsschema. Zo behaal jij sneller je doelen, en kan jij de beste versie van jezelf zijn </p>
+             <h1 className='font-bold text-3xl text-white'>MyFitCamp Pro</h1>
+            <p className='text-[#bcbcbc]  text-lg'>Meten is weten. Met MyFitCamp Pro krijg jij wekelijks een dagelijks voedingplan opgestuurd. Je krijgt een voedingschema, een boodschappenlijstje en de bereidingswijze elke dag in je mail gestuurd. Alle recepten duren slechts 5 minuten om te maken! </p>
             
          </div>
        </section>
