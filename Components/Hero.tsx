1d272ed840501c544c6aafc61737117df35da307
Roy <rjong1033@gmaill.com>
2023-01-18 17:50:14 +0100

    deploy

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Hero() {
   return (
   <>
-    <div className="bg-gray-800 text-white py-16 bg-cover bg-center" style={{backgroundImage: `url('https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-        <div className="container mx-auto">
-            <h2 className="text-3xl font-medium">Join the camp today!</h2>
-            <p className="text-xl font-light">Start now and join the revolution</p>
-            <button className="bg-orange-500 text-black py-2 px-4 rounded-lg hover:bg-orange-600 font-bold">Start jouw body transformatie</button>
+<div className="bg-gray-800 text-white py-16 bg-cover bg-center" style={{backgroundImage: `url('https://images.pexels.com/photos/1640773/pexels-photo-1640773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <div className="container mx-auto text-center">
+            <h2 className="text-3xl font-medium text-black">De online community waar jij fitter van wordt!</h2>
+            <p className="text-xl font-light text-black">Krijg dagelijks voedingsplannen, trainingschema's en bereik jouw doelen sneller. </p>
+            <br />
+            <button className="bg-orange-500 text-black py-2 px-4 rounded-lg hover:bg-orange-600 font-bold">Join het kamp!</button>
         </div>
     </div> 
   </>
--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
 
   return (
     <div className='flex bg-[#232528] text-center gap-3 shadow-xl'>
-      <div className='flex w-[70%] m-auto lg:justify-around md:justify-around xs:flex-col md:flex-row lg:flex-row items-center p-5'>
-        <div className='py-0 flex'>
-            <img src='https://i.ibb.co/rbdwt0r/myfitcamp-1.png ' className='m-auto md:w-[200px] lg:w-[250px] sm:w-[250px] justify-center items-center'></img>
-        </div>
-        </div>
-    </div>
+    <div className='flex w-[70%] m-auto lg:justify-around md:justify-around xs:flex-col md:flex-row lg:flex-row items-center p-5'>
+      <div className='py-0 flex'>
+          <img src='https://i.ibb.co/rbdwt0r/myfitcamp-1.png ' className='m-auto md:w-[200px] lg:w-[250px] sm:w-[250px] justify-center items-center'></img>
+      </div>
+      </div>
+  </div>
   )
 }
