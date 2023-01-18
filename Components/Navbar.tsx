760328a736f958e3ec5c789c62951541cd3b4cff
Roy <rjong1033@gmaill.com>
2023-01-18 15:02:15 +0100

    deploy

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <div className='flex flex-col bg-[#232528] flex-1 items-center text-center p-5 gap-4 py-15 '>
         <div className='py-5 flex flex-col gap-5'>
-            <h1 className='text-white font-bold text-5xl'><span className='text-[#FFA400]'>Neural</span>Meal</h1>
+            <h1 className='text-white font-bold text-5xl'><span className='text-[#FFA400]'>Neural</span>Fit</h1>
             <p className=' text-[20px] text-[#f1f1f1]'>Genereer je persoonlijke voedingplan om jouw droomlichaam te krijgen!</p>
         </div>
         <div className='flex gap-3 items-center '>
