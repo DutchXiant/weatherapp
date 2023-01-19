75e2558cfd59d6eb41e109034b33db0792311f0e
Roy <rjong1033@gmaill.com>
2023-01-19 12:50:34 +0100

    Update hero

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -6,12 +6,12 @@ export default function Hero() {
          <header
         className="w-full h-96 bg-[url('https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-center flex justify-center items-center">
         <div className="flex flex-col justify-center items-center">
-            <h1 className=" text-center text-5xl text-white font-bold drop-shadow-lg">Welkom bij 
+            <h1 className=" text-center lg:text-5xl sm:text-3xl xs:text-3xl text-white font-bold drop-shadow-lg">Jouw droomlichaam begint bij
             <span className="text-[#FFA400]"> MyFitCamp!</span>
             </h1>
             <p className="mt-5 text-center text-lg text-white opacity-70">Weet wat je moet eten om jouw doelen te behalen</p>
             <a className="mt-8 px-12 py-3 bg-gradient-to-r from-[#FFA400] to-red-600 hover:from-amber-600 hover:to-red-700 text-xl text-white/70 font-semibold drop-shadow-lg rounded-full"
-                href="#">Nu starten</a>
+                href="#">Nu starten!</a>
         </div>
     </header>
     </div>
