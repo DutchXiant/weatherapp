1b38583e66c8db4e8048a3b670544ae40822e0cf
Roy <rjong1033@gmaill.com>
2023-01-19 13:47:18 +0100

    Update hero

---
+++ Components/Card.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import { PromiseProvider } from 'mongoose'
+import { useRouter } from 'next/router'
+
+
+export default function Card(props) {
+
+  const {asPath, pathname } = useRouter();
+
+  if(asPath != '/') {
+    return (
+      <div>
+           <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
+                      <h1 className='font-bold text-2xl'>{props.title}</h1>
+                      <p>{props.description}</p>
+                      <Link href="/tools" className='bg-red-500  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>{props.btnText}</Link> 
+            </div>
+      </div>
+    )
+  } 
+
+  if(asPath == '/') {
+    return (
+      <div>
+           <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
+                      <h1 className='font-bold text-2xl'>{props.title}</h1>
+                      <p>{props.description}</p>
+                      <Link href="/tools" className='bg-[#FFA400]  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>{props.btnText}</Link> 
+            </div>
+      </div>
+    )
+  } 
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import Hero from '../Components/Hero'
+import Card from '../Components/Card'
 
 
 
@@ -11,23 +12,11 @@ const Home: NextPage = () => {
         {/* Section Header */ }
         <div className='container width-[85%] m-auto'>
             <section className='flex mt-5 mb-5 xs:flex-col md:flex-col sm:flex-col lg:flex-col xl:flex-row'>
-            
-                <div className='flex flex-col flex-1 text-white p-5 shadow-md  '>
-                   <h1 className='font-bold text-2xl'>Fitness Tools</h1>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum soluta sunt veniam tenetur dicta, qui quas vitae suscipit unde.</p>
-                   <Link href="/tools" className='bg-[#FFA400]  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>Bekijk de tools</Link>
-                </div>
-                <div className='flex flex-col flex-1 text-white p-5 shadow-md '>
-                   <h1 className='font-bold text-2xl'>Community</h1>
-                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti dolore suscipit illo autem ullam facilis odit exercitationem veritatis consequuntur sapiente?</p>
-                   <Link href="/tools" className='bg-[#FFA400]  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>Join discord</Link>
-                </div>
-                <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
-                    <h1 className='font-bold text-2xl'>Gratis leerstof</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, officia. Nesciunt quo incidunt enim fugit! Quisquam labore molestiae mollitia cum?</p>
-                    <Link href="/tools" className='bg-[#FFA400]  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>Download gratis ebooks</Link>
-                </div>
-            </section>
+                {/* All card */}
+                <Card title="Fitness Tools" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, officia. Nesciunt quo incidunt enim fugit! Quisquam labore molestiae mollitia cum?" btnText="Gebruik fitness tools"/>
+                <Card title="Gratis leerstof" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, officia. Nesciunt quo incidunt enim fugit! Quisquam labore molestiae mollitia cum?" btnText="Download ebooks"/>
+                <Card title="Community" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, officia. Nesciunt quo incidunt enim fugit! Quisquam labore molestiae mollitia cum?" btnText="Bekijk community"/> 
+          </section>
 
            
        </div>
