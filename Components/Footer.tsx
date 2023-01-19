cd54a3b1edc280b40fb114b20197157d7545f02a
Roy <rjong1033@gmaill.com>
2023-01-19 12:40:48 +0100

    Update hero

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-[#232528] w-[100%] p-5 flex flex-1 flex-col justify-center '>
+    <div className='bg-black w-[100%] p-5 flex flex-1 flex-col justify-center '>
         <p className="text-white"><b>KVK: </b>Hello</p>
         <p className='text-white'><b>BtW nr: </b>23294934</p>
         <p className='text-white'><b>Adres: </b>Zichtheugte 36</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,51 +7,31 @@ import Hero from '../Components/Hero'
 const Home: NextPage = () => {
   return (
     <>
-    <Hero />
-    <div className='w-[85%] m-auto p-5 flex flex-col flex-1 justify-center items-center'>
-       <section className='flex gap-3 sm:flex-col lg:flex-row md:flex-col xs:flex-col'>
-        <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3  md:m-auto'>
-             <p className='font-bold  text-3xl text-white'>Join ons discord kamp!🧑‍🤝‍🧑</p> 
-             <p className='text-[#bcbcbc] text-lg'>Zoek jij motivatie, of iemand om mee naar de sportschool te gaan? Join dan vandaag de FitCamp Discord. In dit kamp praten we met elkaar over fitness, delen we tips & tricks en delen we onze progressie. Join the camp now!</p>
-             <button className='relative bottom-0 text-lg bg-[#fba400] text-black font-bold py-2'>Join nu!</button>
-            </div>
-          <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3 md:m-auto'>
-            <p className='font-bold text-3xl text-white'>Gepersonaliseerd Trainings Schema🏋️ </p>
-            <p className="text-[#bcbcbc] text-lg">Ontvang binnen 1 minuut een professioneel trainingsschema afsteld op jouw fitness doelen.    </p>
-            <Link href="/trainingsschema" className='bg-[#fba400] py-2 text-black font-bold text-center'> <button>Maak trainingsschema!</button></Link>
-           </div>
-           <div className='p-5 shadow-md md:max-w-[80%] flex flex-col gap-3  md:m-auto'>
-            <p className='font-bold text-3xl text-white'>Gepersonaliseerd voedingschema's📝</p>
-            <p className="text-[#bcbcbc] text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ducimus in harum ea. Sequi adipisci similique dolorum vitae laboriosam deserunt!</p>
-            <button className=' bg-[#fba400] py-2 font-bold text-black'>Maak budget voedingsschema</button>
-           </div>
-       </section>
-       <br />
-       <br />
         <hr  className='text-white'/>
-
-        {/* Section one  */}
-       <section className='flex flex-row gap-9  p-5 xs:flex-col md:flex-row lg:flex-row'>
-         <div className="flex-1   ">
-             <h1 className='font-bold text-3xl text-white'>Elke week een boodschappenlijstje📝</h1>
-            <p className='text-[#bcbcbc] text-lg'>Met MyFitCampPro krijg jij elke week jouw gepersonaliseerde boodschappen lijstje gestuurd in je e-mail. Dit lijstje bevat de voeding die jij nodig hebt om jouw fitness doel te behalen!  </p>
+        <Hero />
+        {/* Section Header */ }
+        <div className='container width-[85%] m-auto'>
+            <section className='flex mt-5 mb-5 xs:flex-col md:flex-col sm:flex-col lg:flex-col xl:flex-row'>
             
-         </div>
-         <div className='flex-1 rounded-xl shadow-3xl'><img  className='rounded-3xl w-[600px]  m-auto items-center shadow-3xl' src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></div>
-       </section>
+                <div className='flex flex-col flex-1 text-white p-5 shadow-md  '>
+                   <h1 className='font-bold text-2xl'>Fitness Tools</h1>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque harum soluta sunt veniam tenetur dicta, qui quas vitae suscipit unde.</p>
+                   <button className='bg-[#FFA400] font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>Bekijk de tools</button>
+                </div>
+                <div className='flex flex-col flex-1 text-white p-5 shadow-md '>
+                   <h1 className='font-bold text-2xl'>Community</h1>
+                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti dolore suscipit illo autem ullam facilis odit exercitationem veritatis consequuntur sapiente?</p>
+                   <button className='bg-[#FFA400] font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3 '>Join discord</button>
+                </div>
+                <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
+                    <h1 className='font-bold text-2xl'>Gratis leerstof</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, officia. Nesciunt quo incidunt enim fugit! Quisquam labore molestiae mollitia cum?</p>
+                    <button className='bg-[#FFA400] font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>Download gratis ebooks</button>
+                </div>
+            </section>
 
-       {/* Section 2 */}
-       <section className='flex flex-row gap-9  p-5 xs:flex-col md:flex-row lg:flex-row'>
-       <div className='flex-1 rounded-xl shadow-3xl xs:order-1 md:order-none'><img  className='rounded-3xl w-[600px]  m-auto items-center shadow-3xl' src="https://images.pexels.com/photos/8154267/pexels-photo-8154267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></div>
-         <div className="flex-1   ">
-             <h1 className='font-bold text-3xl text-white'>MyFitCamp Pro</h1>
-            <p className='text-[#bcbcbc]  text-lg'>Meten is weten. Met MyFitCamp Pro krijg jij wekelijks een dagelijks voedingplan opgestuurd. Je krijgt een voedingschema, een boodschappenlijstje en de bereidingswijze elke dag in je mail gestuurd. Alle recepten duren slechts 5 minuten om te maken! </p>
-            
-         </div>
-       </section>
-
-  
-    </div>
+           
+       </div>
     </>
 
 
