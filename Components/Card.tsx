f1c5539259b081d31c6aad3d810d68eb120d3e3c
Roy <rjong1033@gmaill.com>
2023-01-19 14:07:19 +0100

    Update hero

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -8,18 +8,6 @@ export default function Card(props) {
 
   const {asPath, pathname } = useRouter();
 
-  if(asPath != '/') {
-    return (
-      <div>
-           <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
-                      <h1 className='font-bold text-2xl'>{props.title}</h1>
-                      <p>{props.description}</p>
-                      <Link href="/tools" className='bg-red-500  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>{props.btnText}</Link> 
-            </div>
-      </div>
-    )
-  } 
-
   if(asPath == '/') {
     return (
       <div>
@@ -30,6 +18,13 @@ export default function Card(props) {
             </div>
       </div>
     )
-  } 
-
+      }  return (
+        <div>
+          <div className='flex flex-col flex-1 text-white p-5 shadow-md'> 
+                    <h1 className='font-bold text-2xl'>{props.title}</h1>
+                    <p>{props.description}</p>
+                    <Link href="/tools" className='bg-red-500  text-center font-bold rounded-bl-xl rounded-tr-xl py-1 mt-3'>{props.btnText}</Link> 
+          </div>
+    </div>
+    )
 }
