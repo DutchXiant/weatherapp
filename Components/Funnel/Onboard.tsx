4f9c7e69cf2c3bab5383aab08b814b59a569839b
Roy <rjong1033@gmaill.com>
2023-01-20 14:42:24 +0100

    update

--- Components/Funnel/Onboard.tsx
+++ Components/Funnel/Onboard.tsx
@@ -8,8 +8,6 @@ export default function Onboard() {
   const [selectBody, setSelectedBody] = useState(0);
 
   const clickHandler = (body) => { 
-    console.log(body)
-    //Increasing the count
     const increaseCount = count + 1; 
     setCount(increaseCount)
   }
@@ -17,7 +15,7 @@ export default function Onboard() {
   if(count == 0) { 
     return (
         <div className='w-[85%] m-auto text-center flex flex-col gap-3 items-center mt-5'>
-                   <h1 className='text-white font-bold md:text-4xl lg:text-4xl sm:text-3xl xs:text-3xl'>Stap 1. Selecteer jouw lichaams type</h1>
+                   <h1 className='text-white font-bold md:text-4xl lg:text-4xl sm:text-3xl xs:text-3xl'>Stap 1. Selecteer jouw lichaamstype</h1>
                    <p className="text-gray-400">Vul de korte quiz in en ontvang jouw persoonlijke trainingsschema</p>
                    <div className='m-10 flex gap-10 sm:flex-col xs:flex-col md:flex-row lg:flex-row'>
                        <button onClick={() => clickHandler('1')} className='xs:px-20 border-[#fba503] shadow-2xl shadow-[#fba503] border-2 p-3 rounded-xl'>
@@ -34,18 +32,19 @@ export default function Onboard() {
     )
   }
 
+  
   if(count === 1) { 
     return ( 
         <div className='w-[85%] m-auto text-center flex flex-col gap-3 items-center '>
             <div>
-                <h1 className='text-3xl font-bold text-white mt-5'>Stap 2. Bmi berekenen</h1>
+                <h1 className='text-3xl font-bold text-white mt-5'>Stap 2. Belangrijke gegevens</h1>
                 <br />
                 <div className='flex flex-col gap-3 justify-center flex-1'>
                     <input type="text" className="px-2 py-2" placeholder='Man of vrouw?'></input>
                     <input type="text" className="px-2 py-2" placeholder='Leeftijd'></input>
                     <input type="text" className="px-2 py-2" placeholder='Gewicht'></input>
                     <input type="text" className="px-2 py-2" placeholder='Lengte'></input>
-                    <button className='font-bold px-2 py-1 bg-[#fba503]'>Berekenen</button>
+                    <button className='font-bold px-2 py-1 bg-[#fba503] rounded-xl' onClick={() => clickHandler(1)}>Door naar volgende stap!</button>
                 </div>
                 <div className='flex justity-center mt-10 xs:px-20 border-[#fba503] border-2 p-3 shadow-[#fba503] shadow-2xl rounded-xl'>
                    <Image src="/body1.png" className='m-auto' width={200} height={200} alt="hello"></Image>
@@ -54,6 +53,26 @@ export default function Onboard() {
         </div>
     )
   }
+
+
+
+  if(count === 2) { 
+    return ( 
+        <div className='w-[85%] m-auto text-center flex flex-col gap-3 items-center '>
+            <div>
+                <h1 className='text-3xl font-bold text-white mt-5'>Stap 3. Doel stellen</h1>
+                <p className='text-white'>Jouw BMI is: 52.2. </p>
+                <br />
+                <div className='flex flex-col gap-3 justify-center flex-1'>
+                    <textarea id="w3review" name="w3review" rows="4" cols="50" className='p-2 rounded-xl'></textarea>
+                    <button className='font-bold px-2 py-1 bg-[#fba503] rounded-xl'>Door naar volgende stap!</button>
+                </div>
+              
+            </div>
+        </div>
+    )
+  }
+
 
   
   
