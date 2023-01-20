bd251e1113a1adc7c2c00c960e1db7d6b862d360
Roy <rjong1033@gmaill.com>
2023-01-20 15:47:24 +0100

    hello

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,6 +3,18 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
+       <div className="bg-gray-900 mt-[300px]">
+      <div className="max-w-2xl mx-auto text-white py-10">
+        <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+          <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy;  AventuraMedia. </p>
+          <div className="order-1 md:order-2">
+            <span className="px-2">Over ons</span>
+            <span className="px-2 border-l">Contact</span>
+            <span className="px-2 border-l">Privacy Policy</span>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- Components/Funnel/Onboard.tsx
+++ Components/Funnel/Onboard.tsx
@@ -35,21 +35,17 @@ export default function Onboard() {
   
   if(count === 1) { 
     return ( 
-        <div className='w-[85%] m-auto text-center flex flex-col gap-3 items-center '>
-            <div>
-                <h1 className='text-3xl font-bold text-white mt-5'>Stap 2. Belangrijke gegevens</h1>
-                <br />
-                <div className='flex flex-col gap-3 justify-center flex-1'>
-                    <input type="text" className="px-2 py-2" placeholder='Man of vrouw?'></input>
-                    <input type="text" className="px-2 py-2" placeholder='Leeftijd'></input>
-                    <input type="text" className="px-2 py-2" placeholder='Gewicht'></input>
-                    <input type="text" className="px-2 py-2" placeholder='Lengte'></input>
-                    <button className='font-bold px-2 py-1 bg-[#fba503] rounded-xl' onClick={() => clickHandler(1)}>Door naar volgende stap!</button>
-                </div>
-                <div className='flex justity-center mt-10 xs:px-20 border-[#fba503] border-2 p-3 shadow-[#fba503] shadow-2xl rounded-xl'>
-                   <Image src="/body1.png" className='m-auto' width={200} height={200} alt="hello"></Image>
-                </div>
+        <div className='w-[85%] m-auto text-center flex md:flex-row xs:flex-col sm:flex-col gap-5 items-center  '>
+            <div className='flex  xs:order-2  sm:order-2 md:order-1 lg:order-1 xl:order-1 w-[30%] h-[5%] border-[#fba503] border-2 p-3 shadow-[#fba503] shadow-2xl rounded-xl'>
+                   <Image src="/body1.png" className='items-center m-auto' width={200} height={200} alt="hello"></Image>
             </div>
+            <div className='flex flex-col gap-3 grow  sm:order-1 mt-5'>
+                    <input type="text" className="px-4 py-3  " placeholder='Man of vrouw?'></input>
+                    <input type="text" className="px-4 py-2" placeholder='Leeftijd'></input>
+                    <input type="text" className="px-4 py-3" placeholder='Gewicht'></input>
+                    <input type="text" className="px-4 py-3" placeholder='Lengte'></input>
+                    <button className='font-bold px-2 py-3 bg-[#fba503] rounded-sm' onClick={() => clickHandler(1)}>Door naar volgende stap!</button>
+                </div>
         </div>
     )
   }
