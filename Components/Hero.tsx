ee36c091a95de3a2529925a1d4b11770ac4b3638
Roy <rjong1033@gmaill.com>
2023-01-20 19:46:57 +0100

    hello

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero(props) {
         className={"p-5 w-full h-[300px] bg-[url('https://images.pexels.com/photos/1552103/pexels-photo-1552103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-center flex justify-center items-center"}>
         <div className="flex flex-col justify-center items-center">
             <h1 className=" text-center lg:text-5xl sm:text-3xl xs:text-3xl text-white font-bold drop-shadow-lg">{props.description}
-            <span className="bg-[#FFA400] text-black font-bold"> {props.title}</span>
+            <span className="bg-[#FFA400] text-black font-bold py-1 px-3"> {props.title}</span>
             </h1>
             {asPath == '/' && (
               <div className='flex flex-col items-center justify-center'>
